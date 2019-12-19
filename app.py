31be58e73618a98017cf87d4c47e97f0decd6def
Matheus Alves <matheusalves.aa@outlook.com>
2019-12-19 17:21:50 -0300

    update1

--- app.py
+++ app.py
@@ -1,60 +1,12 @@
 from mathFunctions import *
 from interface import *
 
-def clear():
-    print('\n'*60)
-
 menu()
 while True:
     op = int(input('OPÇÂO: '))
-    if op == 1:
-        numbers = input("Digíte os números a serem calculados: ")
-        numbers = numbers.replace(',', '.')
-        numbers = numbers.split('+')
-        result = soma(*numbers)
-        input(f"Resultado: {result}")
-
-    elif op == 2:
-        numbers = input("Digíte os números a serem calculados: ")
-        numbers = numbers.replace(',', '.')
-        numbers = numbers.split('-')
-        result = sub(*numbers)
-        input(f"Resultado: {result}")
-
-    elif op == 3:
-        numbers = input("Digíte os números a serem calculados: ")
-        numbers = numbers.replace(',', '.')
-        numbers = numbers.split('*')
-        result = mult(*numbers)
-        input(f"Resultado: {result}")
-
-    elif op == 4:
-        numbers = input("Digíte os números a serem calculados: ")
-        numbers = numbers.replace(',', '.')
-        numbers = numbers.split('/')
-        result = div(*numbers)
-        input(f"Resultado: {result}")
-
-    elif op == 5:
-        numbers = input("Digíte os números a serem calculados: ")
-        numbers = numbers.replace(',', '.')
-        numbers = numbers.split('^')
-        result = pot(*numbers)
-        input(f"Resultado: {result}")
-
-    elif op == 6:
-        numbers = input("Digíte os números a serem calculados: ")
-        numbers = numbers.replace(',', '.')
-        result = fatorial(*numbers)
-        input(f"Resultado: {result}")
-
-    elif op == 7:
-        numbers = input("Digíte os números a serem calculados: ")
-        numbers = numbers.replace(',', '.')
-        result = raiz_quadrada(numbers)
-        input(f"Resultado: {result}")
-
-    elif op == 8:
+    if op == 8:
         print('VOLTE SEMPRE')
         break
+    inserir(input("Digíte os números a serem calculados: "), op)
     menu()
+
--- interface.py
+++ interface.py
@@ -1,4 +1,39 @@
+from mathFunctions import *
+
+
 def menu():
     print('='*30)
     print("O que deseja fazer?: \n1 - Soma \n2 - Subtração \n3 - Multiplicação ['*'] \n4 - Divisão \n5 - Potênciação ['^'] \n6 - Fatorização \n7 - Raiz Quadrada \n8 - Encerrar")
     print('='*30)
+
+
+def inserir(txt, opc):
+    numbers = txt
+    numbers = numbers.replace(',', '.')
+    if opc == 1:
+        numbers = numbers.split('+')
+        result = soma(*numbers)
+
+    if opc == 2:
+        numbers = numbers.split('-')
+        result = sub(*numbers)
+
+    if opc == 3:
+        numbers = numbers.split('-')
+        result = mult(*numbers)
+
+    if opc == 4:
+        numbers = numbers.split('/')
+        result = div(*numbers)
+
+    if opc == 5:
+        numbers = numbers.split('^')
+        result = pot(*numbers)
+
+    if opc == 6:
+        result = fatorial(numbers)
+
+    if opc == 7:
+        result = raiz_quadrada(numbers)
+
+    input(f"Resultado: {result}")
